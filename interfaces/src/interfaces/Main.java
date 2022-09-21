049e84a74dbac95bc9e93356aa9d030ae9ede98a
ZkryDds <zkry80@gmail.com>
2022-09-21 18:10:44 +0300

    Interfaces

---
+++ interfaces/src/interfaces/Main.java
@@ -0,0 +1,23 @@
+package interfaces;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		CustomerManager customerManager = new CustomerManager(new MySqlCustomerDal());
+		customerManager.add();
+		
+		/*
+		 * CustomerManager customerManager = new CustomerManager();
+		 * customerManager.customerDal = new MySqlCustomerDal(); customerManager.add();
+		 */
+		
+		
+		/*
+		 * ICustomerDal customerDal = new MySqlCustomerDal(); customerDal.Add();
+		 */
+		
+
+	}
+
+}
